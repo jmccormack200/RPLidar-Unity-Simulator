2dbc106499f57c9845bc8c0907bcffa4bd82709a
John McCormack <mccormack.wgsi@gmail.com>
2015-09-12 17:56:15 -0400

    First commit

---
+++ Assets/RotateLaser.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateLaser : MonoBehaviour {
+	public Vector3 rotationMask = new Vector3(0,1,0);
+	public float rotationSpeed = 50.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate (new Vector3(0,1,0) * rotationSpeed * Time.deltaTime);
+	}
+}
